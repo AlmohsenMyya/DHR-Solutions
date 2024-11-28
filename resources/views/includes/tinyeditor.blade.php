069a834257e1b759b07a6d03673dd37fda91db6f
Bashar Ali <Bashar.Badii.Ali@gmial.com>
2024-11-28 19:11:20 +0300

    Add Project

---
+++ resources/views/includes/tinyeditor.blade.php
@@ -0,0 +1,17 @@
+<script src="https://cdn.tiny.cloud/1/ywrr079zwyazxxpnmrfc0r51u9jfr0m3q9rrzn7ygkrp3u1f/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    var editor_config = {
+        path_absolute : "/",
+        selector: "textarea",
+        plugins: [
+            "advlist autolink lists link charmap print preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime nonbreaking save table contextmenu directionality",
+            "emoticons template paste textcolor colorpicker textpattern"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+        relative_urls: false,
+    };
+
+    tinymce.init(editor_config);
+</script>
